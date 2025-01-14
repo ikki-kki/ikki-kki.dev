306731d0d023a4839256fc756e8274b8d0f4e51e
Yiwei Ho <davidho0403@gmail.com>
2025-01-14 23:43:47 +0800

    feat: footer add github link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,15 @@
+import Github from '@/components/svg/github'
+import Link from 'next/link'
+
 const Footer = () => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <div className="flex justify-center py-8">
+    <div className="flex justify-center items-center space-x-4 py-8">
       <p>1wei.dev - {currentYear}</p>
+      <Link href="https://github.com/1weiho/1wei.dev" target="_blank">
+        <Github />
+      </Link>
     </div>
   )
 }
