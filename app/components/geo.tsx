135c48b6757c2cf7630d82740b92b2965d34d841
Yiwei Ho <davidho0403@gmail.com>
2025-01-14 23:35:55 +0800

    feat: add utc+8 clock

--- app/components/geo.tsx
+++ app/components/geo.tsx
@@ -1,15 +1,19 @@
 import Earth from './earth'
+import UTC8Clock from '@/components/utc-8-clock'
 import Balancer from 'react-wrap-balancer'
 
 const Geo = () => {
   return (
     <div className="flex flex-col md:mt-32 md:flex-row md:space-x-12 justify-center items-center py-32 md:py-40">
+      <UTC8Clock className="md:hidden" />
       <Earth />
-      <div className="max-w-[500px] text-center md:text-start">
+      <div className="max-w-[500px] text-center md:text-start mt-8 md:mt-0">
         <Balancer>
           If you don’t know where Taiwan is, here’s a little reference for you,
           I live here :)
         </Balancer>
+
+        <UTC8Clock className="mt-6 hidden md:block" />
       </div>
     </div>
   )
